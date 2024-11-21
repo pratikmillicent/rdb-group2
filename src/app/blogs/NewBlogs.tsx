35090c2f5c666afddad27901252d3573831da95c
Rajnish Yadav <rajnishyadav5509@gmail.com>
2024-11-21 15:44:36 +0530

    fixes: blogs images changes

--- src/app/blogs/NewBlogs.tsx
+++ src/app/blogs/NewBlogs.tsx
@@ -41,7 +41,7 @@ const NewBlogs = () => {
       date: "August 6, 2022",
       title:
         "Jhamkudi OTT release date ShemarooMe: When to watch this Manasi Parekh-starrer Gujarati blockbuster",
-      image: "https://uithemez.com/i/geekfolio/dark/assets/imgs/blog/h4.jpg",
+      image: "/assets/images3/blogs/movies.jpg",
       tags: ["Marketing", "Design"],
       author: "Admin",
       url: "when-to-watch-jhamkudi-online-ott-shemaroome-release-date-manasi-parekh-gujarati-movie",
@@ -51,7 +51,7 @@ const NewBlogs = () => {
       date: "August 6, 2022",
       title:
         "Jhamkudi Week 1 Box Office: Gujarati horror-com trends extraordinarily; Nets 5.50 crores to emerge a super-hit",
-      image: "https://uithemez.com/i/geekfolio/dark/assets/imgs/blog/h5.jpg",
+      image: "/assets/images3/blogs/movies.jpg",
       tags: ["Marketing", "Design"],
       author: "Admin",
       url: "jhamkudi-week-1-box-office-gujarati-horror-com-trends-extraordinarily-nets-5-50-crores-to-emerge-a-super-hit",
@@ -61,7 +61,7 @@ const NewBlogs = () => {
       date: "August 6, 2022",
       title:
         "Guj CM congratulates ‘Kutch Express’ team for bagging three national awards",
-      image: "https://uithemez.com/i/geekfolio/dark/assets/imgs/blog/h6.jpg",
+      image: "/assets/images3/blogs/movies.jpg",
       tags: ["Marketing", "Design"],
       author: "Admin",
       url: "guj-cm-congratulates-kutch-express-team-for-bagging-three-national-awards",
@@ -71,7 +71,7 @@ const NewBlogs = () => {
       date: "August 6, 2022",
       title:
         "Fractional real estate is the 'new Gold' for savvy festive season investors",
-      image: "https://uithemez.com/i/geekfolio/dark/assets/imgs/blog/h6.jpg",
+      image: "/assets/images3/blogs/building1.jpg",
       tags: ["Marketing", "Design"],
       author: "Admin",
       url: "fractional-real-estate-is-the-new-gold-for-savvy-festive-season-investors",
@@ -81,7 +81,7 @@ const NewBlogs = () => {
       date: "August 6, 2022",
       title:
         "Fractional Ownership: This realty segment to rise over 10 times by 2030 | Here are 3 top hotspots",
-      image: "https://uithemez.com/i/geekfolio/dark/assets/imgs/blog/h6.jpg",
+      image: "assets/images3/blogs/building2.jpg",
       tags: ["Marketing", "Design"],
       author: "Admin",
       url: "fractional-ownership-this-realty-segment-to-rise-over-10-times-by-2030-here-are-3-top-hotspots",
